1e80ff8b520c1678e4aaaba6269f8a07dfccafef
quelvindev <quelvindev@gmail.com>
2024-07-02 23:05:27 -0300

    alterado parametros admin do django

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from contact import models
 
 # Register your models here.
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'id','frist_name','last_name','email', 
+    ordering = '-id',
+    list_filter ='created_date',
+    list_per_page = 20
+    list_max_show_all = 200
+    list_display_links ='id','frist_name',
+    search_fields ='id','frist_name','last_name','email', 
+    
--- contact/models.py
+++ contact/models.py
@@ -5,10 +5,13 @@ from django.utils import timezone
 
 class Contact(models.Model):
 
-    fist_name = models.CharField( max_length=50)
+    frist_name = models.CharField( max_length=50)
     last_name = models.CharField( max_length=50,blank=True)
-    phone = models.CharField(max_length=14)
-    email = models.EmailField(max_length=250)
+    phone = models.CharField(max_length=14,blank=True)
+    email = models.EmailField(max_length=250,blank=True)
     created_date = models.DateTimeField(default=timezone.now)
-    descrition = models.TextField(max_length=500)
-    ...
+    descrition = models.TextField(max_length=500,blank=True)
+    
+    def __str__(self) -> str:
+        return f'{self.frist_name}  {self.last_name}'
+    
