f691c6dc4f775ec6872b1bb7fc0d0ada6eabd204
quelvindev <quelvindev@gmail.com>
2024-07-07 01:24:45 -0300

    adicionado dados aleatorios

--- contact/admin.py
+++ contact/admin.py
@@ -4,13 +4,13 @@ from contact import models
 # Register your models here.
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id','frist_name','last_name','email', 
+    list_display = 'id','first_name','last_name','email', 
     ordering = '-id',
     list_filter ='created_date',
     list_per_page = 20
     list_max_show_all = 200
-    list_display_links ='id','frist_name',
-    search_fields ='id','frist_name','last_name','email', 
+    list_display_links ='id','first_name',
+    search_fields ='id','first_name','last_name','email', 
 
 @admin.register(models.Category)
 class CaregoryAdmin(admin.ModelAdmin):
--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -17,19 +18,23 @@ class Category(models.Model):
 
 class Contact(models.Model):
 
-    frist_name = models.CharField( max_length=50)
+    first_name = models.CharField( max_length=50)
     last_name = models.CharField( max_length=50,blank=True)
     phone = models.CharField(max_length=14,blank=True)
     email = models.EmailField(max_length=250,blank=True)
     created_date = models.DateTimeField(default=timezone.now)
-    descrition = models.TextField(max_length=500,blank=True)
+    description = models.TextField(max_length=500,blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True,upload_to='pictures/%Y/%m/%d')
-    id_caregory = models.ForeignKey(Category,
+    category = models.ForeignKey(Category,
+                                    on_delete=models.SET_NULL,
+                                    blank=True, 
+                                    null=True)
+    onwer = models.ForeignKey(User,
                                     on_delete=models.SET_NULL,
                                     blank=True, 
                                     null=True)
     
     def __str__(self) -> str:
-        return f'{self.frist_name}  {self.last_name}'
+        return f'{self.first_name}  {self.last_name}'
     
