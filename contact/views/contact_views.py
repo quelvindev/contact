9194d2c9e35b57cdccdcbb8c5ae7315b4ff486d4
Quelvin <quelvindev@gmail.com>
2024-07-08 11:11:19 -0300

    passando os contacts para a view

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from contact.models import Contact
 
 #app_name = 'contact'
+contacts = Contact.objects.all()
 
+context = {'contacts':contacts}
 def index(request):
-    return render(request,'contact/index.html')
+    return render(request,'contact/index.html',context)
