b530e883be729955f52ce93977088cc33ddce8f2
quelvindev <quelvindev@gmail.com>
2024-07-07 23:13:57 -0300

    criago package views

--- core/settings.py
+++ core/settings.py
@@ -136,3 +136,10 @@ MEDIA_URL = 'media/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from core.local_settings import *
+except: ImportError
+...
+
+
