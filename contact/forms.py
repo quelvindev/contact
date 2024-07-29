1d73392c89e9215b74af922d012dc56c232444ab
Quelvin <quelvindev@gmail.com>
2024-07-29 15:58:21 -0300

    atualização do form

---
+++ contact/forms.py
@@ -0,0 +1,57 @@
+from typing import Any, Mapping
+from django import forms
+from django.core.exceptions import ValidationError
+from django.core.files.base import File
+from django.db.models.base import Model
+from django.forms.utils import ErrorList
+from contact.models import Contact
+
+
+class ContactForm(forms.ModelForm):
+    first_name = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder':'Rebeca'}),
+        label='Primeiro Nome',
+        help_text='Digite Seu primeiro nome'
+    )
+
+    last_name = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder':'Lohane'}),
+        label='Sobrenome',
+        
+    )
+
+    phone = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder':'75988770024'}),
+        label='Telefone',
+        
+    )
+
+    # ATUALIZANDO O WIDGET
+    # def __init__(self,*args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+
+    #     self.fields['first_name'].widget.attrs.update({
+    #         'placeholder':'Perla Pepa'
+    #     })
+
+    class Meta:
+        model = Contact
+        fields = ('first_name','last_name','phone',)
+        # CRIANDO UM NOVO widgets PARA O CAMPO
+        # widgets= {'first_name': forms.TextInput(
+        #     attrs={'placeholder':'Perla'}
+        # )}
+
+    def clean(self):
+
+        cleaned_data = self.cleaned_data
+
+        self.add_error(
+            None,
+            ValidationError(
+                'Mensagem error',
+                code ='invalid'
+
+            )
+        )
+        return super().clean()
--- contact/views/contact_create.py
+++ contact/views/contact_create.py
@@ -1,15 +1,6 @@
-from django.shortcuts import render,get_object_or_404,redirect
-from contact.models import Contact
-from django.db.models import Q
-from django.http import Http404
-from django.db.models import Max
-from django.core.paginator import Paginator
-from django import forms
+from django.shortcuts import render
+from contact.forms import ContactForm
 
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = ('first_name','last_name','phone',)
 
 
 #app_name = 'contact'
